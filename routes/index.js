560f7691a6ab053df4dca8dfbb6510785034e71f
Telmo Barros <telmo.20@hotmail.com>
2017-10-05 22:41:30 +0100

    admin pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('pages/index', { title: 'Express' });
 });
 
+/* GET admin home page. */
+router.get('/admin', function(req, res, next) {
+    res.render('admin/index', { title: 'Express' });
+});
+
+/* GET admin home page. */
+router.get('/admin/users', function(req, res, next) {
+    res.render('admin/users', { title: 'Express' });
+});
+
 module.exports = router;
