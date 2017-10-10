d8e0fcc1e48cccbb99ae5fb984dcd01b42c82a1f
Telmo Barros <telmo.20@hotmail.com>
2017-10-10 14:49:32 +0100

    create campaign working

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    mongoose = require('mongoose');
+
 
 /* GET campaigns page */
 router.get('/', function(req, res, next) {
@@ -19,7 +21,34 @@ router.get('/create', function(req, res, next) {
 // POST create campaign
 router.post('/create', function (req, res, next) {
     console.log(req.body);
-    res.send('Falta implementar este post, req.body esta a chegar vazio...');
+    mongoose.model('Campaign').create({
+        title : req.body.title,
+        isFunds : req.body.type,
+        description : req.body.description,
+        qtd : req.body.qtd,
+        endDate : req.body.endDate,
+        location : req.body.location
+    }, function (err, campaign) {
+        if (err) {
+            res.send("There was a problem adding the information to the database.\n" + err);
+        } else {
+            //Blob has been created
+            console.log('POST creating new campaign: ' + campaign);
+            res.format({
+                //HTML response will set the location and redirect back to the home page. You could also create a 'success' page if that's your thing
+                html: function(){
+                    // If it worked, set the header so the address bar doesn't still say /adduser
+                    res.location("campaigns");
+                    // And forward to success page
+                    res.redirect("/campaigns");
+                },
+                //JSON response will show the newly created blob
+                json: function(){
+                    res.json(campaign);
+                }
+            });
+        }
+    });
 })
 
 /* GET edit campaign page */
