d7f3c18ee831c821050da70ca09c1c5927e1516f
unknown <jorge-vale1@hotmail.com>
2017-10-18 10:58:47 -0700

    campaigns pages working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    mongoose = require('mongoose'),
+    router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,7 +10,13 @@ router.get('/', function(req, res, next) {
         userLogged = false;
     }
 
-    res.render('pages/index', { userLogged: userLogged });
+    mongoose.model('Campaign').find({isFunds : true}, function(err, fundsCampaigns) {
+        if (err) throw err;
+        mongoose.model('Campaign').find({isFunds : false}, function(err, goodsCampaigns) {
+            if (err) throw err;
+            res.render('pages/index', { fundsCampaigns : fundsCampaigns, goodsCampaigns: goodsCampaigns, userLogged: userLogged });
+        }).limit(3);
+    }).limit(3);
 });
 
 /* GET admin home page. */
@@ -23,15 +30,21 @@ router.get('/admin', function(req, res, next) {
     res.render('admin/index', { userLogged: userLogged });
 });
 
-/* GET admin home page. */
-router.get('/admin/users', function(req, res, next) {
+/* GET campaigns page */
+router.get('/', function(req, res, next) {
     if (req.session.user) {
         userLogged = true;
     } else {
         userLogged = false;
     }
 
-    res.render('admin/users', { userLogged: userLogged });
+    mongoose.model('Campaign').find({"isFunds" :{"$in" : req.body.existingCampaigns}}, function(err, campaigns) {
+        if (err) throw err;
+
+        console.log(campaigns);
+        res.render('pages/index', { campaigns: campaigns, userLogged: userLogged });
+    }).limit(3);
 });
+
 
 module.exports = router;
