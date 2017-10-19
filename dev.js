b0faab5b97b2e55507808e7cae49e0a2b756c3e7
dreamshare <dreamshare>
2017-10-19 02:08:51 +0000

    small fix

--- dev.js
+++ dev.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var session = require('express-session');
+var Cropper = require('cropperjs');
 
 var db = require('./models/devdb');
 var campaignModel = require('./models/campaigns');
